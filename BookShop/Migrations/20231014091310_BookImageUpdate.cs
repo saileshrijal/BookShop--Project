a9cbed293332c90f6bbefaa46b1a01205b93350e
Sailesh Rijal <saileshrijal01@gmail.com>
2023-10-14 16:28:19 +0545

    code cleaned

---
+++ BookShop/Migrations/20231014091310_BookImageUpdate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookImageUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeaturedImage",
+                table: "Books",
+                newName: "FeaturedImagePath");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "BookImages",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "BookImages");
+
+            migrationBuilder.RenameColumn(
+                name: "FeaturedImagePath",
+                table: "Books",
+                newName: "FeaturedImage");
+        }
+    }
+}
